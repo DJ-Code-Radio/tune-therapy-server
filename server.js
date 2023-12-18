8d7347a8098da5cf8b1ef9727ba0acd016bdc00f
Christopher Acosta <chrisacosta12345@gmail.com>
2023-12-18 15:19:00 -0800

    added openai api call for images

--- server.js
+++ server.js
@@ -2,36 +2,44 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors')
-const OpenAI = require('openai');
 const app = express();
-const port = 3000; // or any other port you prefer
+const port = 3000;
+const { generateImage } = require('./controllers/openaiController')
 app.use(cors());
+app.post('/openai/image', generateImage)
 
-const openai = new OpenAI(process.env.OPENAI_API_KEY);
+
+
 //open ai api test for proof of life
+// app.get('/openai', async (req, res) => {
+//   try {
+//     const completion = await openai.chat.completions.create({
+//       messages: [{ role: 'system', content: 'You are a helpful assistant.' }],
+//       model: 'gpt-3.5-turbo',
+//     });
 
-app.get('/openai', async (req, res) => {
-  try {
-    const completion = await openai.chat.completions.create({
-      messages: [{ role: 'system', content: 'You are a helpful assistant.' }],
-      model: 'gpt-3.5-turbo',
-    });
+//     res.json(completion.choices[0]);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: 'Internal Server Error' });
+//   }
+// });
+const getHomePage = (req, res) => {
+  response.status(200).send('Welcome!')
+}
 
-    res.json(completion.choices[0]);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
+app.get('/', getHomePage);
+app.use(express.json());
+app.use('*', (req, res) => {
+  res.status(404).send('These are not the droids you are looking for.');
 });
 
-app.use('*', (req,res) => {
-    res.status(404).send('These are not the droids you are looking for.');
-  });
-  
-  app.use( (error,req,res,next) => {
-    res.status(500).send(`My Bad ... ${error.message}`);
-  });
+app.use((error, req, res, next) => {
+  res.status(500).send(`My Bad ... ${error.message}`);
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+module.exports = app;
