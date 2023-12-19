dd343d5ed6970286c2b1646dd033c6cbef941d37
Errol Vidad <errolvidad@gmail.com>
2023-12-19 23:13:10 +0000

    DRYd controller code and updated with emotion input from client

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -13,94 +13,97 @@ const openai = new OpenAIAPI({
 });
 
 const generateImage = async (req, res) => {
-    try{
-        const imageResponse = await openai.images.generate({ 
-            model: "dall-e-3",
-            prompt: "sadness"
-        });
-            
-    const imageUrl = image.data[0].url;
-    res.json({imageUrl: imageUrl})
-        
-    }catch(e){
-        console.error(e);
-        res.status(500).send("error generating image")
+    try {
+      const { emotion } = req.body;
+      const imageResponse = await openai.images.generate({
+        model: "dall-e-3",
+        prompt: emotion,
+        n: 1,
+      });
+  
+      const imageUrl = imageResponse.data[0].url;
+      res.json({ imageUrl });
+  
+    } catch (e) {
+      console.error(e);
+      res.status(500).send("error generating image");
     }
-};
+  };
+  
 
-const generateImage2 = async () => {
-    try{
-        const image = await openai.images.generate({ 
-            model: "dall-e-3",
-            prompt: "happiness" });
-        imageUrl = image.data[0].url
-        return imageUrl
-    }catch(e){
-        console.error(e)
-    }
-};
+// const generateImage2 = async () => {
+//     try{
+//         const imageResponse = await openai.images.generate({ 
+//             model: "dall-e-3",
+//             prompt: "happiness" });
+//         imageUrl = imageResponse.data[0].url;
+//         return imageUrl
+//     }catch(e){
+//         console.error(e)
+//     }
+// };
 
-const generateImage3 = async () => {
-    try{
-        const image = await openai.images.generate({ 
-            model: "dall-e-3",
-            prompt: "fear" });
-        imageUrl = image.data[0].url
-        return imageUrl
-    }catch(e){
-        console.error(e)
-    }
-};
+// const generateImage3 = async () => {
+//     try{
+//         const imageResponse = await openai.images.generate({ 
+//             model: "dall-e-3",
+//             prompt: "fear" });
+//         imageUrl = imageResponse.data[0].url;
+//         return imageUrl
+//     }catch(e){
+//         console.error(e)
+//     }
+// };
 
-const generateImage4 = async () => {
-    try{
-        const image = await openai.images.generate({ 
-            model: "dall-e-3",
-            prompt: "anger" });
-        imageUrl = image.data[0].url
-        return imageUrl
-    }catch(e){
-        console.error(e)
-    }
-};
+// const generateImage4 = async () => {
+//     try{
+//         const imageResponse = await openai.images.generate({ 
+//             model: "dall-e-3",
+//             prompt: "anger" });
+//         imageUrl = imageResponse.data[0].url;
+//         return imageUrl
+//     }catch(e){
+//         console.error(e)
+//     }
+// };
 
-const generateImage5 = async () => {
-    try{
-        const image = await openai.images.generate({ 
-            model: "dall-e-3",
-            prompt: "surprise" });
-        imageUrl = image.data[0].url
-        return imageUrl
-    }catch(e){
-        console.error(e)
-    }
+// const generateImage5 = async () => {
+//     try{
+//         const imageResponse = await openai.images.generate({ 
+//             model: "dall-e-3",
+//             prompt: "surprise" });
+//         imageUrl = imageResponse.data[0].url;
+//         return imageUrl
+//     }catch(e){
+//         console.error(e)
+//     }
 
-  console.log(image.data);
-};
+//   console.log(image.data);
+// };
 
-const generateImage6 = async () => {
-    try{
-        const image = await openai.images.generate({ 
-            model: "dall-e-3",
-            prompt: "disgust" });
-        imageUrl = image.data[0].url
-        return imageUrl
-    }catch(e){
-        console.error(e)
-    }
-};
+// const generateImage6 = async () => {
+//     try{
+//         const image = await openai.images.generate({ 
+//             model: "dall-e-3",
+//             prompt: "disgust" });
+//         imageUrl = imageResponse.data[0].url;
+//         return imageUrl
+//     }catch(e){
+//         console.error(e)
+//     }
+// };
 
-const generateImage7 = async () => {
-    try{
-        const image = await openai.images.generate({ 
-            model: "dall-e-3",
-            prompt: "trust" });
-        imageUrl = image.data[0].url
-        return imageUrl
-    }catch(e){
-        console.error(e)
-    }
-};
+// const generateImage7 = async () => {
+//     try{
+//         const image = await openai.images.generate({ 
+//             model: "dall-e-3",
+//             prompt: "trust" });
+//         imageUrl = imageResponse.data[0].url;
+//         return imageUrl
+//     }catch(e){
+//         console.error(e)
+//     }
+// };
 
 
 
@@ -138,4 +141,4 @@ const generateImage7 = async () => {
 
 
 
-module.exports = {generateImage, getTrack};
+module.exports = {generateImage};
